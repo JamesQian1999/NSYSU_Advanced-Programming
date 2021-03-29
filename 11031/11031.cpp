5dd4a7ec91509ff8086fb0fcf8957a36f0f5aa6b
jamesqian <jamesqian1999@gmail.com>
2021-03-30 01:02:21 +0800

    just_creat

---
+++ 11031/11031.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,q;
+    do
+    {
+        cin >> n >> q;
+        if(!n && !q) break;
+        int list[n];
+        for(int i=0; i<n; i++)
+            cin >> list[i];
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                
+            }
+        }
+    } while (1);
+    
+}
