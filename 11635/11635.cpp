eb016f2a38a0caa61c954c2dbd94c681c451376f
jamesqian <jamesqian1999@gmail.com>
2021-05-16 01:26:26 +0800

    11635_why_didnt_work

--- 11635/11635.cpp
+++ 11635/11635.cpp
@@ -32,21 +32,14 @@ int main()
         cin.get();
         hotel.push_back(1);
         hotel.push_back(city);
-        //cout << hotel[0] << " " << hotel[1] << " ";
-        while (1)
+        cin >> num;
+        for (int j = 0; j < num; j++)
         {
-            char tmp = cin.get();
-            tolken += tmp;
-            if (tmp == ' ')
-            {
-                hotel.push_back(atoi(tolken.c_str()));
-                //cout << hotel[hotel.size() - 1] << " ";
-                tolken = "";
-            }
-            if (tmp == '\n')
-                break;
+            int tmp;
+            cin >> tmp;
+            hotel.push_back(tmp);
         }
-        //cout << endl << endl;
+
         cin >> num;
         for (int i = 1; i <= num; i++)
         {
@@ -55,40 +48,73 @@ int main()
             cities[a].push_back(c(b, weight));
             cities[b].push_back(c(a, weight));
         }
-        vector<int> hotel2hotel[city + 1];
+
+        int hotel2hotel[hotel.size()][hotel.size()], count[hotel.size()][hotel.size()], ans[hotel.size()][hotel.size()];
         for (int i = 0; i < hotel.size(); i++)
-        //for (int i = 0; i < 1; i++)
         {
-            cout<<endl;
             int bellmen[city + 1];
             for (int j = 1; j <= city; j++)
                 bellmen[j] = 601;
             bellmen[hotel[i]] = 0;
+
             for (int j = 1; j <= city; j++)
-                    cout<<bellmen[j]<<"\t";
-            cout<<endl;
-            for (int j = 1; j <= city; j++)
-            {
                 for (int k = 1; k <= city; k++)
-                {
                     for (int m = 0; m < cities[k].size(); m++)
-                    {
-    
                         if (bellmen[k] > bellmen[cities[k][m].city] + cities[k][m].weight)
-                        {
                             bellmen[k] = bellmen[cities[k][m].city] + cities[k][m].weight;
-                        }
-                    }
-                    cout<<bellmen[k]<<"\t";
-                }
-                cout<<endl;
-            }
-            for (int j = 1; j < hotel.size(); j++)
+
+            for (int j = 0; j < hotel.size(); j++)
             {
+                hotel2hotel[i][j] = 0;
+                count[i][j] = 0;
+                ans[i][j] = 0;
                 if (bellmen[hotel[j]] <= 600)
-                    hotel2hotel[hotel[i]].push_back(hotel[j]);
+                {
+                    hotel2hotel[i][j] = 1;
+                    hotel2hotel[j][i] = 1;
+                    count[i][j] = 1;
+                    count[j][i] = 1;
+                    ans[i][j] = 1;
+                    ans[j][i] = 1;
+                }
             }
         }
+
+        for (int i = 0; i < hotel.size(); i++)
+        {
+            for (int j = 0; j < hotel.size(); j++)
+                cout << hotel2hotel[i][j] << " ";
+            cout << endl;
+        }
+
+
+        int c = 1;
+        if (ans[0][1])
+        {
+            cout << c << endl;
+            continue;
+        }
+        for (int time = 0; time < hotel.size(); time++)
+        {
+            c++;
+            for (int j = 0; j < hotel.size(); j++)
+            {
+                for (int k = 0; k < hotel.size(); k++)
+                {
+                    for (int m = 0; m < hotel.size(); m++)
+                        ans[j][k] += hotel2hotel[k][m] * count[m][k];
+                }
+            }
+            if (ans[0][1])
+                break;
+            for (int j = 0; j < hotel.size(); j++)
+                for (int k = 0; k < hotel.size(); k++)
+                    count[j][k] = ans[j][k];
+        }
+        if (ans[0][1])
+        cout << c << endl;
+        else
+            cout<<"-1"<<endl;
     }
 }
 /*
@@ -103,5 +129,14 @@ int main()
 1 3 375
 2 5 462
 4 6 300
+0 
+
+  |  1   6   2   5   3
+-----------------------
+1 |  1   0   1   0   1
+6 |      1   0   1   0
+2 |          1   1   1
+5 |              1   1
+3 |                  1
 
 */
